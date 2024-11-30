1ce8aa340638edec083d3ad8bc1027d4f99e90cc
AmiTiwarii <amittiwari20012004@gmail.com>
2024-11-30 17:52:53 +0530

    zidio development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
+import Navbar from './components/Header'
+import HeroSection from './components/HeroSection'
+import WhatWeOffer from './components/WhatWeOffer'
+import Testimonials from './components/Testimonial'
+import Footer from './components/Footer'
+import WorkingProcess from './components/WorkingProcess'
+import SoftwareDevelopment from './components/SoftwareDevelopment'
+import TeamMemberCard from './components/TeamMemberCard'
+import AnimeshSingh from "../../public/AnimeshSingh.png"
+import SalmaHusna from "../../public/SalmaHusna.png"
+import ChandanMishra from "../../public/ChandanMishra.png"
+import HarshDash from "../../public/HarshDash.png"
+
+const team = [
+  { name: 'Animesh Singh', role: 'Full Stack Web Developer', image: AnimeshSingh},
+  { name: 'Salma Husna Salsabila', role: 'UI/UX Designer', image: SalmaHusna},
+  { name: 'Chandan Mishra', role: 'Data Scientist', image: ChandanMishra },
+  { name: 'Harsa Dash', role: 'Developer Team Lead', image: HarshDash },
+]
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen bg-gray-900 text-white overflow-hidden">
+      <div className="stars"></div>
+      <div className="twinkling"></div>
+      <Navbar />
+      <HeroSection />
+      <SoftwareDevelopment/>
+      <WhatWeOffer />
+      <div className="flex w-full flex-col p-10">
+        <div className="flex flex-col p-5 pb-10">
+            <span className='bg-blue-400 text-transparent bg-clip-text'>Dedicated Team</span>
+            <span className='font-semibold text-3xl'>Meet Our Dedicated Member</span>
+            <span className='font-semibold text-3xl'>For any enquery</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-row">
+          {team.map((props) => (
+            <TeamMemberCard 
+              key={props.name}
+              name={props.name} 
+              role={props.role}
+              imageSrc={props.image}
+            />
+          ))}
+        </div>
+      </div>
+      <WorkingProcess/>
+      <Testimonials />
+      <Footer />
     </div>
-  );
+  )
 }
